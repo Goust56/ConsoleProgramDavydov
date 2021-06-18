32e6ce315b843f57d98e2c30d651fab9bfe2607f
Cab38-01 <Cab38-01@03-C38-01>
2021-06-18 12:09:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleProgramDavydov/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleProgramDavydov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Написать программный модуль для подсчета количества символов до точки с запятой и после нее";
+            int before = text.IndexOf(';');
+            int after = text.Length - before - 1;
+            Console.WriteLine($"до {before}, после {after}");
+            Console.ReadKey();
+        }
+    }
+}
